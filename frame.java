e026f29915ad4bcc37f44cecdd05e04befd2f69b
yangsc2222 <34789515+yangsc2222@users.noreply.github.com>
2018-01-03 10:38:35 +0800

    Add files via upload

---
+++ frame.java
@@ -0,0 +1,18 @@
+ /**
+     * 判断是否是pc设备
+     */
+    function IsPC() {
+        var userAgentInfo = navigator.userAgent;
+        var Agents = ["Android", "iPhone","SymbianOS", "Windows Phone", "iPod"];
+        var flag = true;
+        for (var v = 0; v < Agents.length; v++) {
+            if (userAgentInfo.indexOf(Agents[v]) > 0) {
+                flag = false;
+                break;
+            }
+        }
+        if(window.screen.width>=768){
+             flag = true;
+        }
+        return flag;    
+     }
